9aad57bda0dc48d1096e07fce16ec015511ab6ba
neruto <neruto.zzz83@gmail.com>
2023-07-06 09:54:24 +0900

    入力アカウント管理画面修正、変更画面のレシポンシブ対応、ボタンの位置修正

--- js/PBL.js
+++ js/PBL.js
@@ -15,23 +15,22 @@
       let name = document.getElementById("myTextBox").value;
       let password = document.getElementById("myTextBox2").value;
       let dropdown = document.getElementById("myDropdown");
-      if (name === "管理者" && password === "oic123") {
+      if (dropdown.value === "ランキング管理画面.html" && name === "管理者" && password === "oic123") {
         let selectedValue = dropdown.value;
         window.location.href = selectedValue;
-      } else if (name === "入力者" && password === "oic987") {
+      } else if (dropdown.value === "入力者管理画面.html" && name === "入力者" && password === "oic987") {
         let selectedValue = dropdown.value;
         window.location.href = selectedValue;
       } else {
         alert("アカウントまたはパスワードが違います。");
       }
     }
-//チーム名管理.html用
-    let textArray = [];  // テキストの配列を管理する変数
+//アカウント管理.html用
     function accountFactory() {
       let addName = document.getElementById("myTextBox");
       let addPass = document.getElementById("myTextBox2");
-      let item1 = document.getElementById("item1");
-      let item2 = document.getElementById("item2");
+      // テーブルのtbody要素を取得
+      let tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
       let checkname= addName.value.trim();
       let checkpass= addPass.value.trim();
       let oicPattern = /^\d+@oic-ok\.ac\.jp$/;
@@ -46,48 +45,47 @@
         alert("8文字で入力してね")
         return;
       }
-        if (checkname !== "") {
-            textArray.push(checkname);  // 配列にテキストを追加
-            item1.innerHTML += "<p onclick=\"selectText(this)\">" + checkname + "</p>";  // テキストを表示
-        }
-        if (checkpass !== "") {
-          textArray.push(checkpass);  // 配列にテキストを追加
-          item2.innerHTML += "<p onclick=\"selectText2(this)\">" + checkpass + "</p>";  // テキストを表示
-      }
+        if (checkname !== "" && checkpass !== "") {
+            // 新しい行を作成
+            var newRow = document.createElement("tr");
+
+            // 入力テキストを表示するセルを作成
+            var nameCell = document.createElement("td");
+            var passCell = document.createElement("td");
+            nameCell.innerText = checkname;
+            passCell.innerText = checkpass;
+
+            // 削除ボタンを表示するセルを作成
+            var deleteCell = document.createElement("td");
+            var deleteButton = document.createElement("button");
+            deleteButton.innerText = "削除";
+            deleteButton.onclick = function() {
+              deleteRow(newRow);
+            };
+            deleteCell.appendChild(deleteButton);
+
+            // 行にセルを追加
+            newRow.appendChild(nameCell);
+            newRow.appendChild(passCell);
+            newRow.appendChild(deleteCell);
+
+            // テーブルに行を追加
+            tableBody.appendChild(newRow);
+
+            // テキストエリアをクリア
+            document.getElementById("myTextBox1").value = "";
+            document.getElementById("myTextBox2").value = "";
+            }
+
+            function deleteRow(row) {
+            var tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+            tableBody.removeChild(row);
+            alert(inputText + "削除しました");
+            }
+
         addName.value = ""; 
         addPass.value = "";  // テキストエリアをクリア
-    }
-    function selectText(element) {
-        element.classList.toggle("selected");  // 選択した項目に"selected"クラスを追加または削除
-    }
-    function selectText2(element) {
-      element.classList.toggle("selected2");  // 選択した項目に"selected"クラスを追加または削除
-  }
-    function deleteButton() {
-        let item1 = document.getElementById("item1");
-        let item2 = document.getElementById("item2");
-        let selectedElements = item1.getElementsByClassName("selected");
-        let selectedElements2 = item2.getElementsByClassName("selected2");
-        while (selectedElements.length > 0) {
-            let selectedElement = selectedElements[0];
-            let selectedText = selectedElement.textContent;
-            let index = textArray.indexOf(selectedText);
-            if (index > -1) {
-                textArray.splice(index, 1);  // 配列からテキストを削除
-            }
-            selectedElement.remove();  // 項目を表示から削除
-        }
-        while (selectedElements2.length > 0) {
-          let selectedElement2 = selectedElements2[0];
-          let selectedText2 = selectedElement2.textContent;
-          let index2 = textArray.indexOf(selectedText2);
-          if (index2 > -1) {
-              textArray.splice(index2, 1);  // 配列からテキストを削除
-          }
-          selectedElement2.remove();  // 項目を表示から削除
-      }
-    }
-    
+    }    
    //競技名管理
    let textArray1 = [];  // テキストの配列を管理する変数
    function sportFactory() {
