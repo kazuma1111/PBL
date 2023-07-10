f5b71a808b9f0598fd50ad8ab2a5b294dda04259
neruto <neruto.zzz83@gmail.com>
2023-07-10 10:37:34 +0900

    入力アカウント管理のjs

--- js/PBL.js
+++ js/PBL.js
@@ -26,6 +26,7 @@
       }
     }
 //入力アカウント管理.html用
+/*
     let textArray = [];  // テキストの配列を管理する変数
     function accountFactory() {
       let addName = document.getElementById("myTextBox");
@@ -87,7 +88,67 @@
           selectedElement2.remove();  // 項目を表示から削除
       }
     }
-    
+    */
+    function accountFactory() {
+      // テキストエリアの値を取得
+      let addName = document.getElementById("myTextBox");
+      let addPass = document.getElementById("myTextBox2");
+      // テーブルのtbody要素を取得
+      let tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+      let checkname= addName.value.trim();
+      let checkpass= addPass.value.trim();
+      let oicPattern = /^\d+@oic-ok\.ac\.jp$/;
+      let numberPattern = /^\d+$/;
+      if (!oicPattern.test(checkname)) {
+        alert('oicのアドレスを入力してください。');
+        return;
+      } else if (!numberPattern.test(checkpass)) {
+        alert('数字のみを入力してください。');
+        return;
+      } else if (addPass.value.length < addPass.maxLength) {
+        alert("8文字で入力してね")
+        return;
+      }
+  
+      if (checkname !== "" && checkpass !== "") {
+  
+      // 新しい行を作成
+      var newRow = document.createElement("tr");
+  
+      // 入力テキストを表示するセルを作成
+      var nameCell = document.createElement("td");
+      var passCell = document.createElement("td");
+      nameCell.innerText = checkname;
+      passCell.innerText = checkpass;
+  
+      // 削除ボタンを表示するセルを作成
+      var deleteCell = document.createElement("td");
+      var deleteButton = document.createElement("button");
+      deleteButton.innerText = "削除";
+      deleteButton.onclick = function() {
+        deleteRow(newRow);
+      };
+      deleteCell.appendChild(deleteButton);
+  
+      // 行にセルを追加
+      newRow.appendChild(nameCell);
+      newRow.appendChild(passCell);
+      newRow.appendChild(deleteCell);
+  
+      // テーブルに行を追加
+      tableBody.appendChild(newRow);
+  
+      // テキストエリアをクリア
+      document.getElementById("myTextBox").value = "";
+      document.getElementById("myTextBox2").value = "";
+      }
+  
+      function deleteRow(row) {
+      var tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+      tableBody.removeChild(row);
+      alert(checkname + "を削除しました");
+      }
+  }
    //競技名管理
    let textArray1 = [];  // テキストの配列を管理する変数
    function sportFactory() {
